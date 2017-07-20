6beb3fa2b3342c82b48f228a454f38df72d7a62c
Rita Ben <yanhong.ben@gmail.com>
2017-07-20 16:45:38 +1000

    test reading file

--- test application/Controllers/HomeController.cs
+++ test application/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using test_application.Models;
+using System.Diagnostics;
 
 namespace test_application.Controllers
 {
@@ -16,8 +18,26 @@ namespace test_application.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
+            List<StaffModels> model = new List<StaffModels>();
 
-            return View();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult About(HttpPostedFileBase postedFile)
+        {
+            List<string> rows = new List<string>();
+            using (System.IO.StreamReader reader = new System.IO.StreamReader(postedFile.InputStream))
+            {
+                while (!reader.EndOfStream)
+                {
+                    rows.Add(reader.ReadLine());
+                }
+            }
+            foreach(var row in rows)
+            {
+                Debug.WriteLine(row);
+            }
+            return RedirectToAction("About");
         }
 
         public ActionResult Contact()
